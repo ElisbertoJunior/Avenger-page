bd247768757d952bcbcd79a112c34bf03aa0f64f
Elisberto Junior <elisberto_jr@hotmail.com>
2023-03-19 02:12:13 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", (btn) => {
+    const targetTab = btn.target.dataset.tabButton;
+    const tab = document.querySelector(`[data-tab-id=${targetTab}]`)
+
+    hideAllTabs();
+    showTabs(tab)
+    removeActiveButton();
+    activeButton(btn);
+    
+  
+    }) 
+
+  })
+
+});
+
+const removeActiveButton  = () => {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+
+  buttons.forEach(button => {
+    button.classList.remove('about-avengers__tabs__button--is-active')
+  })
+}
+
+const activeButton = (btn) => {
+  btn.target.classList.add('about-avengers__tabs__button--is-active')
+}
+
+const hideAllTabs = () => {
+  const tabsContainer = document.querySelectorAll('[data-tab-id]');
+  tabsContainer.forEach(tab =>{
+    tab.classList.remove('about-avengers__list--is-active')
+  })
+}
+
+const showTabs = (tab) => tab.classList.add('about-avengers__list--is-active')
